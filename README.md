659fb8eedef9c720cff943e05b7bd5cc433b935f
EyeEyeloving <tsingsong203@gmail.com>
2023-11-22 10:56:11 +0800

    Add PATCH function and update README file

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # README
+
+This repository shows examples of drawing using MATLAB language.
+
+## Usage
+
+1. For MATLAB versions in R2023a and above, use the `xregion` function; other versions use the `patch` function.
+
+## Additional Resources
+
+None
+
+## Contact
+
+None
--- demo_figure_setting.m
+++ demo_figure_setting.m
@@ -2,28 +2,41 @@
 x = linspace(0,2*pi,100);
 y = sin(x);
 
-%% 创建图窗
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 创建图窗
 figure('Units', 'centimeters', 'Position', [1, 1, 10.5, 9.5]);
 
-%% 定义背景颜色表
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 定义背景颜色表
 CMap = {"#F8E16C", "#FFC2B4"};
 
-%% 绘制子图
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 绘制子图
 x_dist = 0.13;
 y_top = 0.78;
 y_bot = 0.12;
 axes_width = 0.8;
 axes_height = 0.18;
 
-% 子图1
+%%%%%%%%%%%%%%%%%%%%%% 子图1
 f1_ax = subplot(4,1,1); hold(f1_ax, "on");
-xregion(f1_ax, x(1), x(25), 'FaceColor', CMap{1}, 'FaceAlpha', 0.6);
-xregion(f1_ax, x(25), x(50), 'FaceColor', CMap{2}, 'FaceAlpha', 0.6);
-xregion(f1_ax, x(50), x(75), 'FaceColor', CMap{1}, 'FaceAlpha', 0.6);
-xregion(f1_ax, x(75), x(100), 'FaceColor', CMap{2}, 'FaceAlpha', 0.6);
+
+% % MATLAB R2023a 可使用 xregion
+% xregion(f1_ax, x(1), x(25), 'FaceColor', CMap{1}, 'FaceAlpha', 0.6);
+% xregion(f1_ax, x(25), x(50), 'FaceColor', CMap{2}, 'FaceAlpha', 0.6);
+% xregion(f1_ax, x(50), x(75), 'FaceColor', CMap{1}, 'FaceAlpha', 0.6);
+% xregion(f1_ax, x(75), x(100), 'FaceColor', CMap{2}, 'FaceAlpha', 0.6);
+
+% % others 其他版本替换使用 patch
+patch(f1_ax, 'XData', [x(1), x(25), x(25), x(1)], 'YData', [-1, -1, 1, 1], ...
+    'FaceColor', CMap{1}, 'FaceAlpha', 0.6, 'EdgeColor', 'none')
+patch(f1_ax, 'XData', [x(25), x(50), x(50), x(25)], 'YData', [-1, -1, 1, 1], ...
+    'FaceColor', CMap{2}, 'FaceAlpha', 0.6, 'EdgeColor', 'none')
+patch(f1_ax, 'XData', [x(50), x(75), x(75), x(50)], 'YData', [-1, -1, 1, 1], ...
+    'FaceColor', CMap{1}, 'FaceAlpha', 0.6, 'EdgeColor', 'none')
+patch(f1_ax, 'XData', [x(75), x(100), x(100), x(75)], 'YData', [-1, -1, 1, 1], ...
+    'FaceColor', CMap{2}, 'FaceAlpha', 0.6, 'EdgeColor', 'none')
+
 plot(f1_ax, x, y, 'LineWidth', 1, 'Color', 'black');
 
-% 设置子图属性1
+%%%%%%%%%%%%%%%%%%%%%% 设置子图属性1
 % xlabel(f1_ax, 'Time (s)', 'FontName', 'Cambria', 'FontSize', 10);
 ylabel(f1_ax, 'Magnitude', 'FontName', 'Cambria', 'FontSize', 10);
 xlim(f1_ax, [0, 2*pi]);
@@ -33,10 +46,10 @@ set(f1_ax, 'box', 'on', 'LineWidth', 1, ...
     'Position', [x_dist, y_top, axes_width, axes_height]);
 
 
-% 子图2
+%%%%%%%%%%%%%%%%%%%%%% 子图2
 f2_ax = subplot(4,1,2); 
 plot(x, y, 'LineWidth', 1, 'Color', 'black');
-% 设置子图属性2
+%%%%%%%%%%%%%%%%%%%%%% 设置子图属性2
 ylabel(f2_ax, 'Magnitude', 'FontName', 'Cambria', 'FontSize', 10);
 xlim(f2_ax, [0, 2*pi]);
 set(f2_ax, 'box', 'on', 'LineWidth', 1,...
@@ -45,7 +58,7 @@ set(f2_ax, 'box', 'on', 'LineWidth', 1,...
     'Position', [x_dist, (y_top-y_bot)/3*2+y_bot, axes_width, axes_height]);
 
 
-% 子图3
+%%%%%%%%%%%%%%%%%%%%%% 子图3
 f3_ax = subplot(4,1,3); plot(x, y, 'LineWidth', 1, 'Color', 'black');
 % 设置子图属性3
 ylabel(f3_ax, 'Magnitude', 'FontName', 'Cambria', 'FontSize', 10);
@@ -56,9 +69,9 @@ set(f3_ax, 'box', 'on', 'LineWidth', 1,...
     'Position', [x_dist, (y_top-y_bot)/3+y_bot, axes_width, axes_height]);
 
 
-% 子图4
+%%%%%%%%%%%%%%%%%%%%%% 子图4
 f4_ax = subplot(4,1,4); plot(x, y, 'LineWidth', 1, 'Color', 'black');
-% 设置子图属性4
+%%%%%%%%%%%%%%%%%%%%%% 设置子图属性4
 xlabel(f4_ax, 'Time (s)', 'FontName', 'Cambria', 'FontSize', 10);
 ylabel(f4_ax, 'Magnitude', 'FontName', 'Cambria', 'FontSize', 10);
 xlim(f4_ax, [0, 2*pi]);
